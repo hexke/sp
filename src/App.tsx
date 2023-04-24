a63cb7ba5c1d4e2172a55cb8cb7da3af145d758e
Krzysztof_Karowiec <krzysztof.karowiec@gmail.com>
2023-04-24 17:38:27 +0200

    added tariff calculation logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,92 @@
+import { Box, Checkbox, Container, FormControl, FormControlLabel, MenuItem, Select, SelectChangeEvent, Table, TableBody, TableCell, TableFooter, TableHead, TableRow, Typography } from '@mui/material';
 import './App.css'
+import { ChangeEvent } from 'react';
+import { serviceTariff } from './utils/tariff';
+import useTariff from './hooks/useTariff';
 
 function App() {
+    const [changeTariffYear, changeOption, currentTariff, availableOptions, availableYears, total] = useTariff();
 
-  return (
-    <div className="App">
-    </div>
-  );
+    const onYearChange = (e: SelectChangeEvent<string>) => {
+        if (!e.target || !e.target.value || e.target.value === currentTariff.year) {
+            return;
+        }
+        changeTariffYear(e.target.value);
+    }
+
+    const onServiceChange = (e: ChangeEvent<HTMLInputElement>, service: string) => {
+        changeOption(e.target.checked, service);
+    }
+
+    return (
+        <div className="App">
+            <header>
+                <Container sx={{ p: '1rem 0' }}>
+                    <Typography variant='h4' align='center'>Kalkulator Cen</Typography>
+                </Container>
+            </header>
+            <main>
+                <Container sx={{ margin: 'auto', width: 'max-content' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Typography>Rok rozliczeniowy:</Typography>
+                        <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+                            <Select value={currentTariff.year} displayEmpty inputProps={{ 'aria-label': 'Without label' }} onChange={onYearChange}>
+                                {availableYears.map(year => <MenuItem value={year} key={year}>{year}</MenuItem>)}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Usługa</TableCell>
+                                <TableCell>cena&nbsp;(zł)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <FormControlLabel control={<Checkbox onChange={(e) => onServiceChange(e, 'internet')} />} label='Internet' />
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {currentTariff.internet}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>
+                                    <FormControlLabel control={<Checkbox onChange={(e) => onServiceChange(e, 'television')} />} label='telewizja' />
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {currentTariff.television}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>
+                                    <FormControlLabel control={<Checkbox onChange={(e) => onServiceChange(e, 'phone')} />} label='abonament telefoniczny' />
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {currentTariff.phone}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>
+                                    <FormControlLabel control={<Checkbox onChange={(e) => onServiceChange(e, 'decoder')} disabled={!availableOptions.television} />} label='dekoder 4K' />
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {currentTariff.decoder}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                        <TableFooter sx={{ fontSize: '24px' }}>
+                            <TableRow>
+                                <TableCell align='right'>Łącznie:</TableCell>
+                                <TableCell align='center'>{total ? total : '-'}</TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </Container>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/utils/tariff.ts
@@ -0,0 +1,114 @@
+import Tariff from "../interfaces/tariff.interface";
+
+export const serviceTariff: Tariff[] = [
+    {
+        'year': '2023',
+        'internet': 39,
+        'television': 49,
+        'phone': 29,
+        'decoder': 29,
+    },
+    {
+        'year': '2024',
+        'internet': 49,
+        'television': 49,
+        'phone': 29,
+        'decoder': 29,
+    },
+    {
+        'year': '2025',
+        'internet': 59,
+        'television': 59,
+        'phone': 29,
+        'decoder': 29,
+    },
+];
+
+const promotions = [
+    {
+        'year': '2023',
+        'internet + television': 79,
+        'internet + phone': 64
+    },
+    {
+        'year': '2024',
+        'internet + television': 89,
+        'internet + phone': 64
+    },
+    {
+        'year': '2025',
+        'internet + television': 99,
+        'internet + phone': 64
+    },
+]
+
+export const calculateTariff = (year: string, internet: boolean, television: boolean, phone: boolean, decoder: boolean): number => {
+    let total = calculateNoPromotionTariff(year, internet, television, phone, decoder);
+
+    if (internet && television) {
+        let totalWithTelevisionPromotion = calculateTelevisionPromotion(year, phone);
+
+        if (total > totalWithTelevisionPromotion) {
+            total = totalWithTelevisionPromotion;
+        }
+    }
+
+    if (internet && phone) {
+        let totalWithPhonePromotion = calculatePhonePromotion(year, television, decoder);
+
+        if (total > totalWithPhonePromotion) {
+            total = totalWithPhonePromotion;
+        }
+    }
+
+    return total;
+};
+
+const calculateTelevisionPromotion = (year: string, phone: boolean) => {
+    const currentTraffi = serviceTariff.find(traffi => traffi.year === year)!;
+    let total = promotions.find(promotion => promotion.year === year)?.["internet + television"]!;
+
+    if (phone) {
+        total += currentTraffi.phone;
+    }
+
+    return total;
+}
+
+const calculatePhonePromotion = (year: string, television: boolean, decoder: boolean) => {
+    const currentTraffi = serviceTariff.find(traffi => traffi.year === year)!;
+    let total = promotions.find(promotion => promotion.year === year)?.["internet + phone"]!;
+
+    if (television) {
+        total += currentTraffi.television;
+    }
+
+    if (decoder) {
+        total += currentTraffi.decoder;
+    }
+
+    return total;
+}
+
+const calculateNoPromotionTariff = (year: string, internet: boolean, television: boolean, phone: boolean, decoder: boolean): number => {
+    const currentTraffi = serviceTariff.find(traffi => traffi.year === year)!;
+    let total = 0;
+
+    if (internet) {
+        total += currentTraffi.internet;
+    }
+
+    if (television) {
+        total += currentTraffi.television;
+
+        if (decoder) {
+            total += currentTraffi.decoder;
+        }
+    }
+
+    if (phone) {
+        total += currentTraffi.phone;
+    }
+
+    return total;
+}
